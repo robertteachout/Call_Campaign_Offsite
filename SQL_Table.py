a33301040e8c528a6438cb4d99c98ac533068410
Aaron Roethe <roethe.aaron@outlook.com>
2021-07-28 10:14:33 -0700

    Speed up SQL table insert

--- SQL_Table.py
+++ SQL_Table.py
@@ -1,52 +1,104 @@
+from datetime import datetime
 import pandas as pd
 import pyodbc
 import sys
 import numpy as np
 import time
+from Bus_day_calc import next_business_day, Next_N_BD, map_piv, daily_piv, newPath
 
 startTime_1 = time.time()
-
-df = pd.read_csv('C:\\Users\\roeth\\OneDrive - CIOX Health\\Aaron\\Projects\\Call Campaign Automation\\Table_Drop\\Group_Rank.csv', sep=',',low_memory=False)
-df.columns = df.columns.str.replace('/ ','')
-df = df.rename(columns=lambda x: x.replace(' ', "_")).drop(columns='Matches')
-df.iloc[:, 1:] = df.iloc[:, 1:].astype(str)
-# print(df)
-
+Dpath = newPath('Table_Drop','')
+### Load file ###
+df = pd.read_csv(Dpath + 'Group_Rank.csv', sep=',',low_memory=False)
+### Clean ###
+df = df[['OutreachID', 'PhoneNumber', 'Last_Call', 'Score', 'Skill', 'OutreachID_Count', 'Daily_Groups']]
+df['Last_Call'] = df['Last_Call'].fillna(value= pd.to_datetime('10/10/2020'))
+df[['OutreachID', 'Score', 'PhoneNumber', 'OutreachID_Count']] = df[['OutreachID', 'Score', 'PhoneNumber', 'OutreachID_Count']].astype(np.int64)
+df[['Last_Call', 'Daily_Groups']] = df[['Last_Call', 'Daily_Groups']].astype('datetime64[ns]')
+### Server Location ###
+servername = 'HOME\SQLSERVER2019'
 database = 'test_campaign_file'
+# # servername = 'EUS1PCFSNAPDB01'
+# # database = 'DWWorking'
 
-DB = {'servername': 'HOME\SQLSERVER2019',
-    'database': database}
-# create the connection
-conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + DB['servername'] + ';DATABASE=' + DB['database'] + ';Trusted_Connection=yes')
+DB ={
+    'servername': servername,
+    'database'  : database
+    }
 
-cursor = conn.cursor()
+class MyDfInsert:
+    def __init__(self, cnxn, sql_stub, data_frame, rows_per_batch=1000):
+        # NB: hard limit is 1000 for SQL Server table value constructor
+        self._rows_per_batch = 1000 if rows_per_batch > 1000 else rows_per_batch
 
-# # Create Table
-cursor.execute('''DELETE FROM dbo.Campaign''')
+        self._cnxn = cnxn
+        self._sql_stub = sql_stub
+        self._num_columns = None
+        self._row_placeholders = None
+        self._num_rows_previous = None
+        self._all_placeholders = None
+        self._sql = None
 
-# cursor.execute('CREATE TABLE Campaign (OutreachID int, Outreach_Team nvarchar(50), Client_Optum_Category nvarchar(50), Recommended_Schedule_Date nvarchar(50), PhoneNumber nvarchar(50), Address nvarchar(50), City nvarchar(50), State nvarchar(50), Zip nvarchar(50), Agent nvarchar(50), Market nvarchar(50), Project_Due_Date nvarchar(50), TimeZone nvarchar(50), TimeZoneKey nvarchar(50), Site_Clean_Id nvarchar(50), Project_Type nvarchar(50), Audit_Type nvarchar(50), PNP_Code nvarchar(50), ToGoCharts nvarchar(50), TotalCharts nvarchar(50), RetrievedCharts nvarchar(50), CallCount nvarchar(50), Retrieval_Method nvarchar(50), Retrieval_Group nvarchar(50), ScheduleDate nvarchar(50), Age nvarchar(50), Retrieval_Team nvarchar(50), Last_Call nvarchar(50), DaysSinceCreation nvarchar(50), Outreach_Status nvarchar(50), Score nvarchar(50), Skill nvarchar(50), Group_Number nvarchar(50), Cluster nvarchar(50), Load_Date nvarchar(50), Daily_Groups nvarchar(50), OutreachID_Count nvarchar(50), Daily_Priority nvarchar(50), status_sort nvarchar(50), rm_sort nvarchar(50), age_sort nvarchar(50), audit_sort nvarchar(50), TotalChartsAgg nvarchar(50), Cluster_Avg nvarchar(50), Past_Due nvarchar(50), Scheduled nvarchar(50), Unscheduled nvarchar(50), Escalated nvarchar(50), PNP_Released nvarchar(50), Unique_Phone nvarchar(50))')
+        row_count = 0
+        param_list = list()
+        for df_row in data_frame.itertuples():
+            param_list.append(tuple(df_row[1:]))  # omit zero-based row index
+            row_count += 1
+            if row_count >= self._rows_per_batch:
+                self._send_insert(param_list)  # send a full batch
+                row_count = 0
+                param_list = list()
+        self._send_insert(param_list)  # send any remaining rows
 
-# # Insert DataFrame to Table
-for row in df.itertuples():
-    cursor.execute('''
-                    INSERT INTO test_campaign_file.dbo.Campaign
-                    (OutreachID, Outreach_Team, Client_Optum_Category, Recommended_Schedule_Date, PhoneNumber, Address, City, State, Zip, 
-                    Agent, Market, Project_Due_Date, TimeZone, TimeZoneKey, Site_Clean_Id, Project_Type, Audit_Type, PNP_Code, ToGoCharts, 
-                    TotalCharts, RetrievedCharts, CallCount, Retrieval_Method, Retrieval_Group, ScheduleDate, Age, Retrieval_Team, Last_Call, 
-                    DaysSinceCreation, Outreach_Status, Score, Skill, Group_Number, Cluster, Load_Date, Daily_Groups, OutreachID_Count, 
-                    Daily_Priority, status_sort, rm_sort, age_sort, audit_sort, TotalChartsAgg, Cluster_Avg, Past_Due, Scheduled, Unscheduled, 
-                    Escalated, PNP_Released, Unique_Phone)  
-                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
-                    ''', 
-                    row.OutreachID, row.Outreach_Team, row.Client_Optum_Category, row.Recommended_Schedule_Date, row.PhoneNumber, 
-                    row.Address, row.City, row.State, row.Zip, row.Agent, row.Market, row.Project_Due_Date, row.TimeZone, row.TimeZoneKey, 
-                    row.Site_Clean_Id, row.Project_Type, row.Audit_Type, row.PNP_Code, row.ToGoCharts, row.TotalCharts, row.RetrievedCharts, 
-                    row.CallCount, row.Retrieval_Method, row.Retrieval_Group, row.ScheduleDate, row.Age, row.Retrieval_Team, row.Last_Call, 
-                    row.DaysSinceCreation, row.Outreach_Status, row.Score, row.Skill, row.Group_Number, row.Cluster, row.Load_Date, 
-                    row.Daily_Groups, row.OutreachID_Count, row.Daily_Priority, row.status_sort, row.rm_sort, row.age_sort, row.audit_sort, 
-                    row.TotalChartsAgg, row.Cluster_Avg, row.Past_Due, row.Scheduled, row.Unscheduled, row.Escalated, row.PNP_Released, row.Unique_Phone)
+    def _send_insert(self, param_list):
+        if len(param_list) > 0:
+            if self._num_columns is None:
+                # print('[DEBUG] (building items that depend on the number of columns ...)')
+                # this only happens once
+                self._num_columns = len(param_list[0])
+                self._row_placeholders = ','.join(['?' for x in range(self._num_columns)])
+                # e.g. '?,?'
+            num_rows = len(param_list)
+            if num_rows != self._num_rows_previous:
+                # print('[DEBUG] (building items that depend on the number of rows ...)')
+                self._all_placeholders = '({})'.format('),('.join([self._row_placeholders for x in range(num_rows)]))
+                # e.g. '(?,?),(?,?),(?,?)'
+                self._sql = f'{self._sql_stub} VALUES {self._all_placeholders}'
+                self._num_rows_previous = num_rows
+            params = [int(element) if isinstance(element, np.int64) else element
+                      for row_tup in param_list for element in row_tup]
+            # print('[DEBUG]    sql: ' + repr(self._sql))
+            # print('[DEBUG] params: ' + repr(params))
+            crsr = self._cnxn.cursor()
+            crsr.execute(self._sql, params)
 
-conn.commit()
+if __name__ == '__main__':
+    conn_str = (
+        'DRIVER={SQL Server}; SERVER=' + DB['servername'] + '; DATABASE=' + DB['database'] + '; Trusted_Connection=yes'
+    )
+    cnxn = pyodbc.connect(conn_str, autocommit=True)
+    crsr = cnxn.cursor()
+    ### Create Table ###
+    # crsr.execute("""
+    #                 CREATE TABLE Campaign (
+    #                 OutreachID int, PhoneNumber BIGINT, Last_Call date, 
+    #                 Score int, Skill varchar(50), Daily_Groups date, OutreachID_Count int
+    #                 )
+    #                 """)
+
+    t0 = time.time()
+    ### Remove yesterday's file ###
+    crsr.execute('''DELETE FROM dbo.Campaign''')
+    ### Add today's file ###
+    MyDfInsert(cnxn, """
+                    INSERT INTO test_campaign_file.dbo.Campaign (
+                        OutreachID, PhoneNumber, Last_Call, Score, Skill, OutreachID_Count, Daily_Groups) 
+                    """, df, rows_per_batch=275)
+
+    print()
+    print(f'Inserts completed in {time.time() - t0:.2f} seconds.')
+
+    cnxn.close()
 
 executionTime_1 = (time.time() - startTime_1)
 print("-----------------------------------------------")
