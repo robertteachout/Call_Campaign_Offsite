430cdd753afc6bf108f5cf5a6dd22577c68611ca
Aaron Roethe <roethe.aaron@outlook.com>
2021-11-18 14:32:50 -0800

    tables pipeline

--- bin/call_campaign_offsite/pipeline/tables.py
+++ bin/call_campaign_offsite/pipeline/tables.py
@@ -1,29 +1,35 @@
 from pathlib import Path
+import os, sys
+
+from numpy import False_
+path = Path(__file__).parent.absolute().parent.absolute().parent.absolute()
+from datetime import date
+
+today = date.today()
+
 from glob import glob
 import pandas as pd
 import os
-from zipfile import ZipFile
-import zipfile
-import shutil
+
+table_path  = path / "data/table_drop"
+extract     = path / "data/extract"
+load        = path / "data/load"
 
 ### Input/output static tables ###
 def tables(push_pull, table, name):
-    table_path = Path("data/table_drop/")
-    if push_pull == 'push':
-        table.to_csv(table_path / name, sep=',',index=False)
+    if push_pull == 'pull':
+        return pd.read_csv(table_path / name, sep=',',on_bad_lines='warn',engine="python",)
     else:
-        return pd.read_csv(table_path / name, sep=',',low_memory=False)
+        table.to_csv(table_path / name, sep=',', index=False)
 
 ### push_pull zip file ###
-def zipfiles(push_pull, df, filename):
+def zipfiles(push_pull, table, filename):
     if push_pull == 'pull':
-        path = Path("data/extract")
-        Extract_path = list(path.glob(filename))[0]
+        Extract_path = list(extract.glob(filename))[0]
         return pd.read_csv(Extract_path, sep='|', on_bad_lines='warn',engine="python",quoting=3)
     else:
         compression_options = dict(method='zip', archive_name=f'{filename}.csv')
-        p = Path("data/load")
-        df.to_csv(p / f'{filename}.zip', compression=compression_options, sep=',',index=False)
+        table.to_csv(load / f'{filename}.zip', compression=compression_options, sep=',',index=False)
 
 def count_phone(df):
     df0 = tables('pull', 'NA', 'unique_phone_count.csv')
@@ -32,5 +38,7 @@ def count_phone(df):
     gb['%'] = round(gb['Unique_Phone'] / gb['Total'], 2)
     return df0.append(gb, ignore_index=True)
 
-
-
+if __name__ == "__main__":
+    filename = str(f'Call_Campaign_v4_{today.strftime("%m%d")}*')
+    load = zipfiles('pull', 'NA', filename)
+    print(len(load))
