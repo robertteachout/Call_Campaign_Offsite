8140ac5280c266a013e82a657ec0ce183ec6bb9c
Aaron Roethe <roethe.aaron@outlook.com>
2021-11-19 10:54:09 -0800

    logging config timestamp

--- bin/call_campaign_offsite/log/log.py
+++ bin/call_campaign_offsite/log/log.py
@@ -1,6 +1,10 @@
 import logging
 
-logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(filename='app.log',
+                    filemode='w',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 def generator():
     yield 
