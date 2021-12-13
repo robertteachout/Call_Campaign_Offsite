ab782fce0913d80f0ed06fe2fab7ff1f5c201eda
Aaron Roethe <roethe.aaron@outlook.com>
2021-12-13 13:45:53 -0700

    log update formate

--- call_campaign_offsite/log/log.py
+++ call_campaign_offsite/log/log.py
@@ -6,10 +6,9 @@ logging.basicConfig(filename='app.log',
                     level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-def df_len(df):
-    logging.info(f'len: {len(df)}')
+def df_len(name, df):
+    logging.info(f'len: {len(df)} \t\t {name}')
     # logging.info(df.dtypes)
-
 
 if __file__ == '__main__':
     df = [1,2,3,4]
--- call_campaign_offsite/main.py
+++ call_campaign_offsite/main.py
@@ -11,11 +11,16 @@ import pipeline.new_inventory
 import pipeline.rolled_inventory
 from pipeline.etc import daily_piv, time_check, next_business_day, x_Bus_Day_ago, Next_N_BD, date_list_split
 from pipeline.tables import tables, zipfiles, count_phone
+
 import server.insert
+import server.secret
 import server.query
-import server.query_reschedule
-import server.query_lc_org_search
+import server.queries.reschedule
+import server.queries.last_call_search
 import log.log as log
+
+servername  = server.secret.servername
+database    = server.secret.database
 
 date_format = '%Y-%m-%d'
 startTime_1 = time.time()
@@ -25,7 +30,7 @@ tomorrow_str = tomorrow.strftime(date_format)
 
 B10 = Next_N_BD(today, 10)
 
-def full_campaign_file():
+def main():
     ### 1 -> create new two 2 sprint schedule
     ### 0 -> run daily campaign
     start = tables('pull', 'NA', 'start.csv')
@@ -39,28 +44,28 @@ def full_campaign_file():
     ### Get data and mutate
     filename = str(f'Call_Campaign_v4_{today.strftime("%m%d")}*')
     load = zipfiles('pull', 'NA', filename)
-    log.df_len(load)
+    log.df_len('load', load)
     
     ### Add to query
-    reschedule_sql = server.query_reschedule.sql()
-    reSchedule = server.query.query('DWWorking', reschedule_sql, 'Add reschedules')
-    log.df_len(reSchedule)
+    reschedule_sql = server.queries.reschedule.sql()
+    reSchedule = server.query.query(servername, database,  reschedule_sql, 'Add reschedules')
+    log.df_len('reSchedule', reSchedule)
 
     df_full0 = load.append(reSchedule, ignore_index = True)
-    log.df_len(df_full0)
+    log.df_len('df_full', df_full0)
 
     clean= pipeline.clean.clean(df_full0, tomorrow_str)
-    log.df_len(clean)
+    log.df_len('clean', clean)
 
     anthem = tables('pull', 'NA', 'anthem.csv')
-    log.df_len(anthem)
+    log.df_len('anthem', anthem)
 
     skilled = pipeline.skills.complex_skills(clean, x_Bus_Day_ago(3), anthem)
-    log.df_len(skilled)
+    log.df_len('skilled', skilled)
     skilled['PhoneNumber'] = skilled['PhoneNumber'].astype(str)
 
     df_full, test0 = pipeline.clean.check_load(skilled, today)
-    log.df_len(df_full)
+    log.df_len('df_full', df_full)
     time_check(startTime_1, f'File Load \t{test0}')
 
     ### Add Schedule -- Daily Groups 
@@ -73,42 +78,42 @@ def full_campaign_file():
     else: 
         assignment = tables('pull', 'NA','assignment_map.csv' )
         mapped, names = pipeline.sprint_schedule.daily_maping(df_full, assignment, tomorrow_str)
-        log.df_len(mapped)
+        log.df_len('mapped', mapped)
 
         ### Add dynamic daily group based on remaining sprint ###
         load_balanced_inv = pipeline.new_inventory.NewID_sprint_load_balance(mapped,tomorrow_str,remaining_bd)
-        log.df_len(load_balanced_inv)
+        log.df_len('load balance', load_balanced_inv)
 
         min=[3,Day]
         min.sort()
         if Day != 0:
-            lc_search_sql = server.query_lc_org_search.sql(CF=x_Bus_Day_ago(min[0]), NIC=x_Bus_Day_ago(Day),lbd=x_Bus_Day_ago(1))
-            lc_search = server.query.query('DWWorking', lc_search_sql, 'Last Call Check')
-            log.df_len(lc_search)
+            lc_search_sql = server.queries.last_call_search.sql(CF=x_Bus_Day_ago(min[0]), NIC=x_Bus_Day_ago(Day),lbd=x_Bus_Day_ago(1))
+            lc_search = server.query.query(servername, database, lc_search_sql, 'Last Call Check')
+            log.df_len('last call', lc_search)
         
             rolled, list_add = pipeline.rolled_inventory.check_lc(load_balanced_inv,lc_search, Day, tomorrow_str)
-            log.df_len(rolled)
+            log.df_len('rolled', rolled)
         else:
             rolled = load_balanced_inv 
             list_add = pd.DataFrame()
 
         clean_for_score = pipeline.sprint_schedule.map_priotiy(rolled, Day, names)
-        log.df_len(clean_for_score)
+        log.df_len('prep to score', clean_for_score)
     time_check(startTime_1, 'Sprint Schedule')
     
     df_scored = pipeline.score.split_drop_score(clean_for_score)
-    log.df_len(df_scored)
+    log.df_len('scored', df_scored)
     time_check(startTime_1, 'Split, Score, & Parent/Child Relationship')
     
     orignal_inv = tables('pull','NA',"assignment_map.csv")
-    log.df_len(orignal_inv)
+    log.df_len('og inv', orignal_inv)
 
     NewID = pipeline.new_inventory.append_new_inventory(df_scored)
-    log.df_len(NewID)
+    log.df_len('newId', NewID)
 
     orignal_inv = tables('pull','NA',"assignment_map.csv")
     add_inv = orignal_inv.append(NewID)
-    log.df_len(add_inv)
+    log.df_len('addInv', add_inv)
     time_check(startTime_1, 'Add NewIDs to list')
 
     def Save():
@@ -124,11 +129,11 @@ def full_campaign_file():
         daily_piv(df_scored)
         time_check(startTime_1, 'Create Pivot Table')
         ####################################
-        if test0 == 'Fail':
-            pass
-        else:
-            Save()
-            server.insert.batch_insert(x_Bus_Day_ago(10).strftime("%Y-%m-%d"),tomorrow_str, df_scored)
+        # if test0 == 'Fail':
+        #     pass
+        # else:
+        #     Save()
+        #     server.insert.batch_insert(servername, database, x_Bus_Day_ago(10).strftime(date_format), tomorrow_str, df_scored)
         time_check(startTime_1, 'batch_insert')
  
     ###calculate ever 2 weeks
@@ -139,7 +144,7 @@ def full_campaign_file():
         tables('push', df_key, 'assignment_map.csv')
         dt = pipeline.sprint_schedule.static_schedule(B10)
         tables('push', dt, 'start.csv')
-        full_campaign_file()
+        main()
 
 if __name__ == "__main__":
-    full_campaign_file()
+    main()
