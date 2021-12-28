87c41225588c0536680b2c93889c7f0b3905dea0
Aaron Roethe <roethe.aaron@outlook.com>
2021-12-28 11:36:09 -0700

    setup, export env

--- setup.bat
+++ setup.bat
@@ -3,5 +3,6 @@ call %USERPROFILE%\Anaconda3\Scripts\activate %USERPROFILE%\Anaconda3
 call activate base
 
 call conda env create --file environment.yml
+@REM call conda env export > environment.yml
 call activate %venv%
 PAUSE
