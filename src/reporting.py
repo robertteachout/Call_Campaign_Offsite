e9c0f126a58644a744b4879d8f8e0308c0184215
Aaron Roethe <roethe.aaron@outlook.com>
2022-01-19 08:44:02 -0700

    aca reporting automation

--- src/reporting.py
+++ src/reporting.py
@@ -1,10 +1,14 @@
 import reports.optum_care
+import reports.optum_aca
 
-report = reports.optum_care.main()
+optum_care = reports.optum_care.main()
+optum_aca = reports.optum_aca.main()
 import pandas as pd
 
-df1 = pd.read_csv('docs/report_oc.csv',index_col='Total')
-c= pd.merge(df1, report, how='left', left_index=True,right_index=True)
+report_oc_full = pd.read_csv('docs/report_oc.csv',index_col='Total')
+report_aca_full = pd.read_csv('docs/report_aca.csv',index_col='Total')
+oc= pd.merge(report_oc_full, optum_care, how='left', left_index=True, right_index=True)
+aca= pd.merge(report_aca_full, optum_aca, how='left', left_index=True, right_index=True)
 
-c.to_csv('docs/report_oc.csv')
-
+oc.to_csv('docs/report_oc.csv')
+aca.to_csv('docs/report_aca.csv')
