d7b30cfc9e66059cf69b99e145eebfb7fddd10db
Aaron Roethe <roethe.aaron@outlook.com>
2022-01-27 15:38:47 -0700

    reporting, add campaign_disp to mainstream

--- src/reporting.py
+++ src/reporting.py
@@ -3,14 +3,16 @@ import pandas as pd
 import reports.project_breakdown
 import reports.project_breakdown_full
 import reports.campaign_project_score
+import reports.campaign_disp
 
 def main():
     oc_name  = ['AHN','CDQI HCR','NAMMCA','OC-AZ','OC-NV','OCN-WA','OC-UT','Reliant','Riverside', 'WellMed']
     aca_name = ['ACA-HospitalCR','ACA-PhysicianCR']
 
-    reports.project_breakdown.main(oc_name, 'docs/report_oc.csv')
-    reports.project_breakdown.main(aca_name, 'docs/report_aca.csv')
-    reports.campaign_project_score.main()
+    # reports.project_breakdown.main(oc_name, 'docs/report_oc.csv')
+    # reports.project_breakdown.main(aca_name, 'docs/report_aca.csv')
+    # reports.campaign_project_score.main('2022-01-27')
+    reports.campaign_disp.main(projects=aca_name)
 
 if __name__ == '__main__':
     main()
