bbd9421f1f1106b6a9068fc967934eb45852dc6a
Aaron Roethe <roethe.aaron@outlook.com>
2022-01-27 15:37:32 -0700

    reporting, streamline campaign dispostion

--- src/reports/campaign_disp.py
+++ src/reports/campaign_disp.py
@@ -3,6 +3,8 @@ import pandas as pd
 from datetime import date
 from pipeline.etc import last_business_day
 from server.query import query
+from pipeline.tables import append_column
+
 import server.queries.call_conversion
 
 import server.secret
@@ -14,12 +16,13 @@ today_str = today.strftime('%Y-%m-%d')
 yesterday = last_business_day(today)
 yesterday_str = yesterday.strftime("%Y-%m-%d")
 
-def main(day, projects=list()):
+def main(projects=list(),day=yesterday_str):
     # sql extract
     def data(lbd):
         sql = server.queries.call_conversion.sql(lbd)
         df = query(servername, database, sql, '')
         return df
+
     df = data(day)
     # filter for projects
     df = df[df.ProjectType.isin(projects)].copy()
@@ -31,17 +34,18 @@ def main(day, projects=list()):
     f1 = df.Disp_Name.notna()
     df['disp_count'] = np.where(f1, 1,0)
     # aggregate
-    totals = df[['OutreachID','disp_count','cf_call','cf_no_call']].agg(
+    totals = df.agg(
         {'OutreachID':'count','disp_count':'sum','cf_call':'sum','cf_no_call':'sum'}
         ).to_frame().rename(columns={0:day})
-
-    project = df.groupby('ProjectType')['OutreachID'].count().to_frame(
+    # create pivots
+    def grouper(group):
+        return df.groupby(group)['OutreachID'].count().to_frame(
         ).sort_values(by="OutreachID",ascending=False
         ).rename(columns={"OutreachID":f"{day}"})
 
-    disp = df.groupby('Disp_Name')['OutreachID'].count().to_frame(
-        ).sort_values(by="OutreachID",ascending=False
-        ).rename(columns={"OutreachID":f"{day}"})
+    project = grouper('ProjectType')
+    disp = grouper('Disp_Name')
+
     # consolidate top dispositions
     top = 10
     disp_clean = disp[:top].copy()
@@ -49,9 +53,8 @@ def main(day, projects=list()):
     # add breaks for appending all three df's
     project_break = pd.DataFrame(disp.columns,columns=disp.columns, index=['Projects'])
     disp_break = pd.DataFrame(disp.columns,columns=disp.columns, index=['Disposition'])
-
-    return (totals.append(project_break).append(project).append(disp_break).append(disp_clean))
-if __name__ == '__main__':
-    print('test')
-    # projects = ['ACA-PhysicianCR','ACA-HospitalCR']
-    # main(projects)
+    # append to one df
+    final = (totals.append(project_break).append(project).append(disp_break).append(disp_clean))
+    final.index.rename('Totals', inplace=True)
+    # create and store new data to this location
+    append_column(final, 'docs/aca_historical_reporting.csv',['Totals'])
