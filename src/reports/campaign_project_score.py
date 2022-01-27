112d899f5bd6c71a7a5a8fe86a85634c3687b2a0
Aaron Roethe <roethe.aaron@outlook.com>
2022-01-27 14:15:19 -0700

    reporting, campaign_score add child&parent orgs

--- src/reports/campaign_project_score.py
+++ src/reports/campaign_project_score.py
@@ -1,36 +1,41 @@
-from audioop import tomono
-import imp
 import pandas as pd
 import numpy as np
-from pathlib import Path
 from datetime import datetime, date
 
 from pipeline.tables import tables, append_column
-from pipeline.etc import last_business_day
+from pipeline.etc import last_business_day, next_business_day
 
 today = date.today()
 today_str = today.strftime("%Y-%m-%d") 
 
-tomorrow = last_business_day(today)
+yesterday = last_business_day(today)
+yesterday_str = yesterday.strftime("%Y-%m-%d") 
+
+tomorrow = next_business_day(today)
 tomorrow_str = tomorrow.strftime("%Y-%m-%d") 
 
-def main():
-    df = tables('pull', 'na', f'{today}.zip','data/load/')
+def main(today_str):
+    df = tables('pull', 'na', f'{today_str}.zip','data/load/')
+    work = {
+        'CC_Tier1':1500,
+        'CC_Tier2':5000,
+        'CC_Adhoc1':1800,
+        'CC_Adhoc2':1000
+    }
+    filters = '|'.join([f"(Skill == '{i}' & Score < {j})" for i, j in work.items()])
+    skills = '|'.join([f"(Skill == '{i}')" for i in work.keys()])
+    top_num = df.query(filters).PhoneNumber.tolist()
     
-    f1 = (df.Skill == 'CC_Tier1') & (df.Score < 1500)
-    f2 = (df.Skill == 'CC_Tier2') & (df.Score < 5000)
-    f3 = (df.Skill == 'CC_Adhoc1') & (df.Score < 1000)
-    f4 = (df.Skill == 'CC_Adhoc2') & (df.Score < 1000)
-    df['Called'] = np.where(f1 | f2 | f3 | f4, 1, 0)
-
-    called = df[df.Called == 1].pivot_table(
-                                        index=['Skill','Project_Type'], 
-                                        values ='OutreachID', 
-                                        aggfunc = ['count'])
+    table = df[df.PhoneNumber.isin(top_num)]
+    tb = table.query(skills).copy()
+    called = tb.pivot_table(
+                            index=['Skill','Project_Type'], 
+                            values ='OutreachID', 
+                            aggfunc = ['count'])
 
     called.columns = called.columns.droplevel()
-    name = tomorrow_str
+    name = today_str
     called.columns = [name]
-    called = called.sort_values(name,ascending=False)
-    append_column(called, 'docs/campaign_project_score.csv', ['Skill', 'Project_Type'])
-
+    print(called)
+    append_column(called, 'data/daily_priority/campaign_project_score.csv', ['Skill', 'Project_Type'])
+    tb[['Skill','OutreachID', 'PhoneNumber', 'Score','Last_Call']].to_csv(f'data/daily_priority/{today_str}.csv', index=False)
