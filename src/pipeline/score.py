d18b549ade4fce0c26d6403ad624e840a89e312a
Aaron Roethe <roethe.aaron@outlook.com>
2022-02-03 16:27:19 -0700

    rank, audit stack merge

--- src/pipeline/score.py
+++ src/pipeline/score.py
@@ -11,23 +11,29 @@ def rank(df):
     # f5 = df.Skill == 'CC_Tier2'
     # df['temp_rank'] = np.where(f1 & (f2 | f3) & f4 | f0, 0,1)
     df['temp_rank'] = np.where(f0 & (f2 | f3) & f4, 0,1)
-    return df.sort_values(
-        by = ['temp_rank', 'togo_bin', 'age'], 
-        # by = ['meet_sla','temp_rank', 'togo_bin', 'age'], 
-        ascending=[True,False, False]
-        # ascending=[True, True,False, False]
-        ).reset_index(drop = True)
+    df_sort = df.sort_values(['Audit_Type']) \
+        .groupby(['Audit_Type']) \
+        .apply(lambda x: x.sort_values(['meet_sla', 'togo_bin', 'age']
+            ,ascending=[True, False, False])) \
+        .reset_index(drop=True)
+
+    df_sort.Score = df_sort.groupby(['Audit_Type']) \
+        .Score.rank('first')
+    
+    return df_sort.sort_values(['Score', 'Audit_Type']).reset_index(drop=True)
 
 def split(df, sk):
     # new score
     df0 = df[df['Skill'] == sk].copy()
     scored = rank(df0)
+    
     non_dup = scored.drop_duplicates(['PhoneNumber']).reset_index(drop = True)
     df_skill = rank(non_dup)
-    
+
     df_skill['Unique_Phone'] = 1
     ### add score column
     df_skill['Score'] = range(len(df_skill))
+
     # Add Unique ORGs to Rank list 
     df5 = df_skill.append(scored)
     df6 = df5.drop_duplicates(['OutreachID']).reset_index(drop= True)
