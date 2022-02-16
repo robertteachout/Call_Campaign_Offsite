18ce7d04f8ffc0243e1457ec492fca0d153526ed
Aaron Roethe <roethe.aaron@outlook.com>
2022-02-16 16:09:50 -0700

    main, remove reschedules add comments

--- src/main.py
+++ src/main.py
@@ -6,13 +6,10 @@ import pipeline.clean
 import pipeline.score
 import pipeline.sprint_schedule
 import pipeline.skills
-from pipeline.etc import daily_piv, time_check, next_business_day, x_Bus_Day_ago, Next_N_BD, date_list_split
+from pipeline.etc import daily_piv, time_check, next_business_day, x_Bus_Day_ago
 from pipeline.tables import tables, zipfiles, count_phone
 
-import server.insert
-import server.secret
-import server.query
-import server.queries.reschedule
+import server.query, server.insert, server.secret
 import server.queries.mastersiteID
 import log.log as log
 
@@ -29,61 +26,53 @@ def main():
     load = zipfiles('pull', 'NA', filename)
     log.df_len('load', load)
 
-    # test last call date = today
+    ### test last call date = today
     tested, test = pipeline.clean.check_load(load, today)
     log.df_len('tested', tested)
     time_check(startTime_1, f'File Load \t{test}')
     
     ### Master Site ID
-    mastersite_sql = server.queries.mastersiteID.sql()
-    mastersite = server.query.query(servername, database,  mastersite_sql, 'Add mastersiteID')
-    tables('push',  mastersite,     'mastersite.csv')
-    # mastersite = tables('pull',  'na',     'mastersite.csv')
+    # mastersite_sql = server.queries.mastersiteID.sql()
+    # mastersite = server.query.query(servername, database,  mastersite_sql, 'Add mastersiteID')
+    # tables('push',  mastersite,     'mastersite.csv')
+    mastersite = tables('pull',  'na',     'mastersite.csv')
     mapped = pd.merge(tested, mastersite, how='left', on='OutreachID')
     log.df_len('mastersiteID', mapped)
     time_check(startTime_1, 'msid map')
 
-    ## Reschedules
-    # reschedule_sql = server.queries.reschedule.sql()
-    # reSchedule = server.query.query(servername, database,  reschedule_sql, 'Add reschedules')
-    # log.df_len('reSchedule', reSchedule)
-
-    # df_full0 = load.append(reSchedule, ignore_index = True)
-    # log.df_len('df_full', df_full0)
-
-    # fix & add columns
+    ### fix & add columns
     clean = pipeline.clean.clean(mapped, tomorrow_str)
     log.df_len('clean', clean)
     time_check(startTime_1, 'clean')
 
-    # reskill inventory
+    ### reskill inventory
     skilled = pipeline.skills.complex_skills(clean)
     log.df_len('skilled', skilled)
     skilled['PhoneNumber'] = skilled['PhoneNumber'].astype(str)
     time_check(startTime_1, 'skill')
 
-    # score inventory per skill
+    ### score inventory per skill
     scored = pipeline.score.split(skilled)
     log.df_len('scored', scored)
     time_check(startTime_1, 'Split, Score, & Parent/Child Relationship')
     
-    # get column name & types ~ collect unique phone script
+    ### get column name & types ~ collect unique phone script
     column_types = scored.dtypes.reset_index()
     # uniq_num_count = count_phone(scored)
 
     def Save():
-        # save files
         zipfiles('push', scored, tomorrow_str)
         # tables('push',  uniq_num_count,     'unique_phone_count.csv')
         tables('push',  column_types,       'columns.csv')
         time_check(startTime_1, 'Save files')
-        # insert into server
+        ### insert into server ###
         server.insert.batch_insert(servername, database, x_Bus_Day_ago(10).strftime(date_format), tomorrow_str, scored)
         time_check(startTime_1, 'batch_insert')
 
-    # create campaign pivot
+    ### create campaign pivot
     daily_piv(scored)
     time_check(startTime_1, 'Create Pivot Table')
+    # zipfiles('push', scored, tomorrow_str)
 
     if test == 'Pass': Save() 
 
