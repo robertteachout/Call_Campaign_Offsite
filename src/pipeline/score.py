f51e8236113f25b5368f3bb5462a3be2ca1389cc
Aaron Roethe <roethe.aaron@outlook.com>
2022-02-16 16:08:38 -0700

    rank & stack_inv, clean universal func

--- src/pipeline/score.py
+++ src/pipeline/score.py
@@ -1,41 +1,60 @@
 import pandas as pd
 import numpy as np
 
-def rank(df, group, new_col):
-    df.sort_values(['Skill', group,'meet_sla','temp_rank', 'no_call', 'togo_bin', 'age']
-               ,ascending=[True, True, True, False, False, False, False], inplace=True)
+# def rank(df, group, new_col):
+#     df.sort_values(['Skill', group,'meet_sla','temp_rank', 'no_call', 'togo_bin', 'age']
+#                ,ascending=[True, True, True, False, False, False, False], inplace=True)
+#     df[new_col] = 1
+#     df[new_col] = df.groupby(['Skill', group])[new_col].cumsum()
+#     return df
+
+def rank(df=pd.DataFrame, new_col=str, groups=list, rank_cols=dict):
+    sort_columns = groups + [*rank_cols.keys()]
+    ascending    = [True] * len(groups) + [*rank_cols.values()]
+    
+    df.sort_values(sort_columns, ascending=ascending, inplace=True)
     df[new_col] = 1
-    df[new_col] = df.groupby(['Skill', group])[new_col].cumsum()
+    df[new_col] = df.groupby(groups)[new_col].cumsum()
     return df
 
-def stack_inventory(df, grouping='PhoneNumber'):
+def stack_inventory(df, grouping):
+    rank_cols = {'meet_sla':True,'temp_rank':False, 'no_call':False, 'togo_bin':False, 'age':False}
+    if grouping == 'mastersiteID':
+        f0 = df.Project_Type.isin(['ACA-PhysicianCR']) # 'ACA-PhysicianCR'
+        f1 = df.Project_Type.isin(['UHC HEDIS']) # 'ACA-PhysicianCR'
+        df['temp_rank'] = np.where(f1, 1, 0)
+        df['temp_rank'] = np.where(f0, 2, df.temp_rank)
     # group by phone number or msid & rank highest value org
-    df = rank(df, grouping  , 'overall_rank')
+    df = rank(df,'overall_rank',['Skill',grouping], rank_cols)
 
     # top overall per group = parent
     f1 = df.overall_rank == 1
     df['parent'] = np.where(f1, 1, 0)
 
     # re-rank parent orgs
-    df = rank(df, 'parent'  , 'Score')
+    df = rank(df,'Score'       , ['Skill','parent'], rank_cols)
     df['Matchees'] = df.groupby([grouping])['OutreachID'].transform(lambda x : '|'.join(x)).apply(lambda x: x[:3000])
     return df
 
 def split(df):
     df['OutreachID'] = df['OutreachID'].astype(str)
-    f0 = df.Project_Type.isin(['UHC HEDIS','HEDIS']) # 'ACA-PhysicianCR'
+    f0 = df.Project_Type.isin(['UHC HEDIS','ACA-PhysicianCR']) # 'ACA-PhysicianCR'
+    # f1 = df.Project_Type.isin(['ACA-PhysicianCR']) # 'ACA-PhysicianCR'
     df['temp_rank'] = np.where(f0, 1, 0)
+    # df['temp_rank'] = np.where(f1, 1, df['temp_rank'])
 
     split = 'CC_Cross_Reference'
     notmsid = df[df.Skill != split].copy()
     msid    = df[df.Skill == split].copy()
 
-    scored      = stack_inventory(notmsid)
-    msid_scored = stack_inventory(msid, grouping='mastersiteID')
+    scored      = stack_inventory(notmsid, 'PhoneNumber')
+    msid_scored = stack_inventory(msid, 'mastersiteID')
 
     dubs = scored.append(msid_scored)
     unique = dubs.drop_duplicates(['OutreachID']).reset_index(drop= True)
     ### Piped ORGs attached to phone numbers
+    f0 = df.Project_Type.isin(['Chart Sync']) # 'ACA-PhysicianCR'
+    df['Score'] = np.where(f0, 1000000, df.Score)
     return unique
         
 def split_drop_score(df):
