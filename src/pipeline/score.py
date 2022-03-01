b0da631249297b54d260b90914c4dd7c66b64838
Aaron Roethe <roethe.aaron@outlook.com>
2022-02-28 22:11:54 -0700

    project_rank, dynmic project temp order

--- src/pipeline/score.py
+++ src/pipeline/score.py
@@ -11,31 +11,36 @@ def rank(df=pd.DataFrame, new_col=str, groups=list, rank_cols=dict):
     df[new_col] = df.groupby(groups)[new_col].cumsum()
     return df
 
-def stack_inventory(df, grouping):
-    rank_cols = {'meet_target_sla':True, 'temp_rank':False, 'temp_rank2':False,'no_call':False,'age':False} # 'togo_bin':False, 
-    if grouping == 'PhoneNumber':
-        f0 = df.Project_Type.isin(['HEDIS','Chart Review']) # 'ACA-PhysicianCR'
-        df['temp_rank2'] = np.where(f0, 1, 0)
+def project_rank(df, project, rank):
+    f1 = df.Project_Type == project # 'ACA-PhysicianCR'
+    df[f'temp_rank{rank}'] = np.where(f1, 1, 0)
+    return df
 
+def stack_inventory(df, grouping):
+    projects = ['UHC HEDIS', 'ACA-PhysicianCR', 'HEDIS', 'Chart Review']
+    temp = {}
+    for index, project in enumerate(projects):
+        # create column on dataframe
+        df = project_rank(df, project, index)
+        # create dict for scoring
+        temp[f'temp_rank{index}'] = False
+
+    rank_cols = {'meet_target_sla':True, **temp, 'no_call':False, 'togo_bin':False, 'age':False} 
     # group by phone number or msid & rank highest value org
-    df = rank(df,'overall_rank',['Skill',grouping], rank_cols)
+    df = rank(df,'overall_rank',['Skill', grouping], rank_cols)
 
     # top overall per group = parent
     f1 = df.overall_rank == 1
     df['parent'] = np.where(f1, 1, 0)
 
     # re-rank parent orgs
-    df = rank(df,'Score'       , ['Skill','parent'], rank_cols)
+    df = rank(df,'Score', ['Skill','parent'], rank_cols)
+
     df['Matchees'] = df.groupby([grouping])['OutreachID'].transform(lambda x : '|'.join(x)).apply(lambda x: x[:3000])
     return df
 
 def split(df):
     df['OutreachID'] = df['OutreachID'].astype(str)
-    # f0 = df.Project_Type.isin(['UHC HEDIS','ACA-PhysicianCR']) # 'ACA-PhysicianCR'
-    f0 = df.Project_Type.isin(['UHC HEDIS']) # 'ACA-PhysicianCR'
-    f1 = df.Project_Type.isin(['ACA-PhysicianCR']) # 'ACA-PhysicianCR'
-    df['temp_rank'] = np.where(f0, 1, 0)
-    df['temp_rank2'] = np.where(f1, 1, 0)
 
     split = 'CC_Cross_Reference'
     notmsid = df[df.Skill != split].copy()
@@ -47,8 +52,8 @@ def split(df):
     dubs = scored.append(msid_scored)
     unique = dubs.drop_duplicates(['OutreachID']).reset_index(drop= True)
     ### Piped ORGs attached to phone numbers
-    f0 = df.Project_Type.isin(['Chart Sync']) # 'ACA-PhysicianCR'
-    df['Score'] = np.where(f0, 1000000, df.Score)
+    f0 = unique.Project_Type.isin(['Chart Sync']) # 'ACA-PhysicianCR'
+    unique['Score'] = np.where(f0, 1000000, unique.Score)
     return unique
         
 def split_drop_score(df):
