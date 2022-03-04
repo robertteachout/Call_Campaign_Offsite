be26d6f314edc6383d0a1441eb15c80265e82964
Aaron Roethe <roethe.aaron@outlook.com>
2022-03-03 19:56:46 -0700

    business_line class, capacity model master rank

--- src/pipeline/score.py
+++ src/pipeline/score.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from dataclasses import dataclass
 
 
 def rank(df=pd.DataFrame, new_col=str, groups=list, rank_cols=dict):
@@ -11,17 +12,29 @@ def rank(df=pd.DataFrame, new_col=str, groups=list, rank_cols=dict):
     df[new_col] = df.groupby(groups)[new_col].cumsum()
     return df
 
-def project_rank(df, project, rank):
-    f1 = df.Project_Type == project # 'ACA-PhysicianCR'
+def project_rank(df, buz_line, rank):
+    f1 = df.Project_Type.isin(buz_line.projects) # 'ACA-PhysicianCR'
     df[f'temp_rank{rank}'] = np.where(f1, 1, 0)
     return df
 
+@dataclass
+class business_lines:
+    projects: list[str]
+    capacity: int
+    system:str
+
 def stack_inventory(df, grouping):
-    projects = ['UHC HEDIS', 'ACA-PhysicianCR', 'HEDIS', 'Chart Review']
+    uhc     = business_lines(['UHC HEDIS'], 3650,'CC_ChartFinder')
+    hedis   = business_lines(['HEDIS'], 2450,'CC_ChartFinder')
+    aca     = business_lines(['ACA-PhysicianCR'], 3550,'CC_ChartFinder')
+    medicaid= business_lines(['Chart Review','Clinical Review MCaid PhyCR'], 2500,'CC_Cross_Reference')
+
+    business = [uhc, hedis, aca, medicaid]
+
     temp = {}
-    for index, project in enumerate(projects):
+    for index, buz_line in enumerate(business):
         # create column on dataframe
-        df = project_rank(df, project, index)
+        df = project_rank(df, buz_line, index)
         # create dict for scoring
         temp[f'temp_rank{index}'] = False
 
@@ -35,13 +48,40 @@ def stack_inventory(df, grouping):
 
     # re-rank parent orgs
     df = rank(df,'Score', ['Skill','parent'], rank_cols)
+    
+    if grouping == 'PhoneNumber':  
+        for buz_line in business:
+            if buz_line.system == 'CC_Cross_Reference':
+                pass
+            else:
+                f0 = df.Skill == buz_line.system
+                f1 = df.Project_Type.isin(buz_line.projects)
+                f2 = df.parent == 1
+                f3 = df.meet_target_sla == 0
 
+                orgs = df[f0 & f1 & f2 & f3].sort_values('Score')['OutreachID'].head(buz_line.capacity).to_list()
+                print(f'"{buz_line.projects}"\t target: {buz_line.capacity} == {len(orgs)}')
+                try:
+                    assert len(orgs) == buz_line.capacity
+                except:
+                    exit()
+
+                f4 = df.OutreachID.isin(orgs)
+                df.master = np.where(f4, 1, df.master)
+
+        new_rank_cols = {'meet_target_sla':True, 'master':False, **temp, 'no_call':False, 'togo_bin':False, 'age':False} 
+        df = rank(df,'Score', ['Skill'], new_rank_cols)
+    
     df['Matchees'] = df.groupby([grouping])['OutreachID'].transform(lambda x : '|'.join(x)).apply(lambda x: x[:3000])
     return df
 
 def split(df):
     df['OutreachID'] = df['OutreachID'].astype(str)
-
+    df['master'] = 0
+    f1 = df.Project_Type == 'UHC HEDIS'
+    piv = df[f1]
+    piv2 = piv.pivot_table(index=['Skill','meet_target_sla','no_call'], values=['OutreachID'], aggfunc='count')
+    print(piv2)
     split = 'CC_Cross_Reference'
     notmsid = df[df.Skill != split].copy()
     msid    = df[df.Skill == split].copy()
