a288074e7a50c49456478ca1b5bb9a7d46f039ac
Aaron Roethe <roethe.aaron@outlook.com>
2022-03-08 18:07:31 -0700

    stack_inventory, last faze timezone scoring

--- src/pipeline/score.py
+++ src/pipeline/score.py
@@ -2,6 +2,12 @@ import pandas as pd
 import numpy as np
 from dataclasses import dataclass
 
+@dataclass
+class business_lines:
+    projects: list
+    capacity: int
+    system:str
+    skill:str
 
 def rank(df=pd.DataFrame, new_col=str, groups=list, rank_cols=dict):
     sort_columns = groups + [*rank_cols.keys()]
@@ -17,18 +23,11 @@ def project_rank(df, buz_line, rank):
     df[f'temp_rank{rank}'] = np.where(f1, 1, 0)
     return df
 
-@dataclass
-class business_lines:
-    projects: list
-    capacity: int
-    system:str
-    skill:str
-
 def stack_inventory(df, grouping):
     uhc     = business_lines(['UHC HEDIS'],3650,'CC_ChartFinder','CC_Adhoc3')
     hedis   = business_lines(['HEDIS'], 2450,'CC_ChartFinder','CC_Adhoc4')
     aca     = business_lines(['ACA-PhysicianCR'], 3550,'CC_ChartFinder','CC_Adhoc5')
-    medicaid= business_lines(['Chart Review','Clinical Review MCaid PhyCR'], 2500,'CC_ChartFinder','CC_Adhoc6')
+    medicaid= business_lines(['Chart Review','Clinical Review MCaid PhyCR','RADV'], 2500,'CC_ChartFinder','CC_Adhoc6')
 
     business = [uhc, hedis, aca, medicaid]
 
@@ -55,28 +54,15 @@ def stack_inventory(df, grouping):
             if buz_line.system == 'CC_ChartFinder':
                 f0 = df.Skill == buz_line.system
                 f1 = df.Project_Type.isin(buz_line.projects)
-                # f2 = df.parent == 1
                 df.Skill = np.where(f0 & f1, buz_line.skill, df.Skill)
 
-    #             orgs = df[f0 & f1 & f2 & f3].sort_values('Score')['OutreachID'].head(buz_line.capacity).to_list()
-    #             print(f'"{buz_line.projects}"\t target: {buz_line.capacity} == {len(orgs)}')
-    #             try:
-    #                 assert len(orgs) == buz_line.capacity
-    #             except:
-    #                 exit()
-
-    #             f4 = df.OutreachID.isin(orgs)
-    #             df.master = np.where(f4, 1, df.master)
-
-    #     new_rank_cols = {'meet_target_sla':True, 'master':False, **temp, 'no_call':False, 'togo_bin':False, 'age':False} 
-    #     df = rank(df,'Score', ['Skill'], new_rank_cols)
-    
+    rank_final = {'meet_target_sla':True, 'timezone_sort':True, 'no_call':False,'age':False, 'togo_bin':False} 
+    df = rank(df,'Score', ['Skill','parent'], rank_final)
     df['Matchees'] = df.groupby(['Skill', grouping])['OutreachID'].transform(lambda x : '|'.join(x)).apply(lambda x: x[:3000])
     return df
 
 def split(df):
     df['OutreachID'] = df['OutreachID'].astype(str)
-    df['master'] = 0
    
     split = 'CC_Cross_Reference'
     notmsid = df[df.Skill != split].copy()
@@ -88,8 +74,8 @@ def split(df):
     dubs = scored.append(msid_scored)
     unique = dubs.drop_duplicates(['OutreachID']).reset_index(drop= True)
     ### Piped ORGs attached to phone numbers
-    f0 = unique.Project_Type.isin(['Chart Sync']) # 'ACA-PhysicianCR'
-    unique['Score'] = np.where(f0, 1000000, unique.Score)
+    # f0 = unique.Project_Type.isin(['Chart Sync']) # 'ACA-PhysicianCR'
+    # unique['Score'] = np.where(f0, 1000000, unique.Score)
     return unique
         
 def split_drop_score(df):
