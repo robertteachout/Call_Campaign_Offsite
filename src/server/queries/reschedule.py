dc9beb18e5be2c0b6e67c1efc63e93c7f00f6357
Aaron Roethe <roethe.aaron@outlook.com>
2022-03-10 10:11:17 -0700

    reschedule query update, new data format

--- src/server/queries/reschedule.py
+++ src/server/queries/reschedule.py
@@ -1,32 +1,29 @@
 def sql():
 	sql = """
 	SELECT DISTINCT
-		 m.[Outreach ID]										AS [OutreachID]
+         m.[Outreach ID]										AS [OutreachID]
 		,m.[Audit Type] 										
 		,m.[Project Type] 										
 		,m.[Project Due Date] 									
-		,m.[Outreach Status] 									
-		, lcd.Date										        [Last Call]
-		, s.Phone1                                              AS  [PhoneNumber]
-		-- , csid.SiteCleanId                                      AS  [Site Clean ID]
-		, CONCAT(S.Address1, ' ', S.Address2)                   AS  [Address]
-		, S.City												AS	[City]
-		, S.State												AS  [State]
-		, S.Zip	 
+		,m.[Outreach Status] 	                                							
+        ,ISNULL(CONVERT(VARCHAR(10), OD.LastCallDate, 1), '')   AS [Last Call]
+        ,SC.PHONENUM                                            AS [PhoneNumer]
+
 	FROM DWWorking.Prod.Master_Reporting_Table                  AS m
 	LEFT JOIN DW_Operations.dbo.DimOutreach			    		AS	om
 		ON m.[Outreach ID] = om.OutreachId
-	JOIN DW_Operations.dbo.DimDate						        AS lcd
-		ON om.LastCallDateId = lcd.DateKey
-	JOIN [ChartFinder_Snap].[dbo].[Site]                        AS	csid
-		ON om.PrimarySiteId = csid.ID
-	JOIN DW_Operations.dbo.DimSite					            AS	s
-		ON om.PrimarySiteId=s.SiteId
-		
+	LEFT JOIN ChartFinder.dbo.ProjectSite PS
+        ON OM.PrimarySiteId = PS.SiteId
+            AND PS.OutreachID = OM.OutreachID
+    LEFT JOIN ChartFinder.dbo.SiteContact SC
+        ON sc.id = PS.PrimaryContactId
+	LEFT JOIN ChartFinder.dbo.OutreachDates OD 
+        ON OM.OutreachID = OD.OutreachID
 	WHERE   m.[Outreach status] = 'ReSchedule'
 		AND m.[Project Type] <> 'On Demand'
 		AND m.[Project Status] <> 'Completed'
 		AND m.[Retrieval Group] = 'Offsite'
 		AND m.[Project Due Date] > GETDATE()
+        AND sc.PrimaryFlag = 1
 	"""
 	return sql
