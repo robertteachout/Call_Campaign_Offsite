1c931269a1f516de8b0e640e9ee3870c55537fed
Aaron <roethe.aaron@outlook.com>
2022-03-27 23:05:08 -0700

    Server abstact base class, create_engine cohesion

--- src/server/insert.py
+++ src/server/insert.py
@@ -55,23 +55,6 @@ class MyDfInsert:
             crsr = self._cnxn.cursor()
             crsr.execute(self._sql, params)
 
-
-class Server(ABC):
-    @abstractmethod
-    def create_engine():
-        pass
-
-@dataclass
-class MSSQL:
-    server  : str
-    database: str
-    driver  : str = "ODBC Driver 17 for SQL Server"
-    engine  : sqlalchemy.engine = field(init=False)
-
-    def __post_init__(self) -> None:
-        self.engine = sqlalchemy.create_engine(
-        f"mssql+pyodbc://{self.server}/{self.database}?driver={self.driver}", fast_executemany=True)
-
 def clean_for_insert(load):
     load.rename({'parent':'Unique_Phone'}, axis=1, inplace=True)
     df = load[['OutreachID', 'PhoneNumber', 'Score', 'Skill','Unique_Phone','Load_Date','MasterSiteId']].copy()
@@ -88,11 +71,35 @@ def clean_for_insert(load):
     date_key    = dict.fromkeys(date_cols, 'datetime64[ns]')
     return df.astype(dict(int_key, **date_key))
 
-def before_insert(server:MSSQL, remove, lookup) -> None:
-    server.engine.execute(remove)
-    print(pd.read_sql(lookup, dwworking.engine))
+class Server(ABC):
+    server  : str
+    database: str
+    driver  : str 
+    
+    @abstractmethod
+    def create_engine():
+        pass
 
-def sql_insert(load, server: MSSQL, table):
+@dataclass
+class MSSQL(Server):
+    server  : str
+    database: str
+    driver  : str = "ODBC Driver 17 for SQL Server"
+
+    def create_engine(self) -> sqlalchemy.engine:
+        return sqlalchemy.create_engine(
+            f"mssql+pyodbc://{self.server}/{self.database}?driver={self.driver}", fast_executemany=True)
+    
+    # def __post_init__(self) -> None:
+    #     self.engine = sqlalchemy.create_engine(
+    #     f"mssql+pyodbc://{self.server}/{self.database}?driver={self.driver}", fast_executemany=True)
+
+
+def before_insert(engine:sqlalchemy.engine, remove, lookup) -> None:
+    engine.execute(remove)
+    print(pd.read_sql(lookup, engine))
+
+def sql_insert(load, engine:sqlalchemy.engine, table):
     # ask to go forward with insert
     if input("Enter(y/n): ") == 'y':
         pass
@@ -101,7 +108,7 @@ def sql_insert(load, server: MSSQL, table):
     ### Load file ###
     t0 = time.time()
     ### Add today's file #
-    load.to_sql(table, server.engine, index=False, if_exists="append")
+    load.to_sql(table, engine, index=False, if_exists="append")
     print(f'Inserts completed in {time.time() - t0:.2f} seconds.')
 
 if __name__ == "__main__":
@@ -123,9 +130,10 @@ if __name__ == "__main__":
     table       = 'Call_Campaign'
 
     dwworking   = MSSQL(server, database)
+    dw_engine = dwworking.create_engine()
 
     load = clean_for_insert(df)
     load_date = ''.join(df.Load_Date.unique())
     remove, lookup = sql(x_Bus_Day_ago(10), load_date)
-    before_insert(dwworking, remove, lookup)
-    sql_insert(load, dwworking, table)
+    before_insert(dw_engine, remove, lookup)
+    sql_insert(load, dw_engine, table)
