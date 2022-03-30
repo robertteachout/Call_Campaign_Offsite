c0509cbf75f30ca3b65affac349ec4c00b6e3ef9
Aaron Roethe <roethe.aaron@outlook.com>
2022-03-29 17:01:12 -0700

    MyDfInsert mega class, removed and optimized

--- src/server/insert.py
+++ src/server/insert.py
@@ -1,59 +1,9 @@
-from abc import ABC, abstractmethod
-from dataclasses import dataclass, field
 import pandas as pd
 import sys, os
 import numpy as np
 import time
 import sqlalchemy
-
-from datetime import date
 from pathlib import Path
-
-class MyDfInsert:
-    def __init__(self, cnxn, sql_stub, data_frame, rows_per_batch=1000):
-        # NB: hard limit is 1000 for SQL Server table value constructor
-        self._rows_per_batch = 1000 if rows_per_batch > 1000 else rows_per_batch
-
-        self._cnxn = cnxn
-        self._sql_stub = sql_stub
-        self._num_columns = None
-        self._row_placeholders = None
-        self._num_rows_previous = None
-        self._all_placeholders = None
-        self._sql = None
-
-        row_count = 0
-        param_list = list()
-        for df_row in data_frame.itertuples():
-            param_list.append(tuple(df_row[1:]))  # omit zero-based row index
-            row_count += 1
-            if row_count >= self._rows_per_batch:
-                self._send_insert(param_list)  # send a full batch
-                row_count = 0
-                param_list = list()
-        self._send_insert(param_list)  # send any remaining rows
-
-    def _send_insert(self, param_list):
-        if len(param_list) > 0:
-            if self._num_columns is None:
-                # print('[DEBUG] (building items that depend on the number of columns ...)')
-                # this only happens once
-                self._num_columns = len(param_list[0])
-                self._row_placeholders = ','.join(['?' for x in range(self._num_columns)])
-                # e.g. '?,?'
-            num_rows = len(param_list)
-            if num_rows != self._num_rows_previous:
-                # print('[DEBUG] (building items that depend on the number of rows ...)')
-                self._all_placeholders = '({})'.format('),('.join([self._row_placeholders for x in range(num_rows)]))
-                # e.g. '(?,?),(?,?),(?,?)'
-                self._sql = f'{self._sql_stub} VALUES {self._all_placeholders}'
-                self._num_rows_previous = num_rows
-            params = [int(element) if isinstance(element, np.int64) else element
-                    for row_tup in param_list for element in row_tup]
-            # print('[DEBUG]    sql: ' + repr(self._sql))
-            # print('[DEBUG] params: ' + repr(params))
-            crsr = self._cnxn.cursor()
-            crsr.execute(self._sql, params)
 
 def clean_for_insert(load):
     load.rename({'parent':'Unique_Phone'}, axis=1, inplace=True)
@@ -70,26 +20,6 @@ def clean_for_insert(load):
     date_cols   = ['Daily_Groups', 'Load_Date']
     date_key    = dict.fromkeys(date_cols, 'datetime64[ns]')
     return df.astype(dict(int_key, **date_key))
-
-class Server(ABC):
-    server  : str
-    database: str
-    driver  : str 
-    
-    @abstractmethod
-    def create_engine():
-        pass
-
-@dataclass
-class MSSQL(Server):
-    server  : str
-    database: str
-    driver  : str = "ODBC Driver 17 for SQL Server"
-
-    def create_engine(self):
-        return sqlalchemy.create_engine(
-            f"mssql+pyodbc://{self.server}/{self.database}?driver={self.driver}"
-            , fast_executemany=True)
 
 def before_insert(engine:sqlalchemy.engine, remove, lookup) -> None:
     engine.execute(remove)
@@ -113,6 +43,7 @@ if __name__ == "__main__":
     sys.path.append(str(package_root_directory))
     from pipeline.etc import Business_Days, x_Bus_Day_ago
     from server.queries.call_campaign_insert import sql
+    from server.connections import MSSQL
     buzday = Business_Days()
     
     day = buzday.tomorrow_str
