77f606deb9a617a08ca8fc68dff80644ff3712ea
Aaron Roethe <roethe.aaron@outlook.com>
2022-04-04 15:51:49 -0700

    extract_file_name, compressed_files,rm optum query

--- src/main.py
+++ src/main.py
@@ -1,27 +1,21 @@
 import pandas as pd
-import numpy as np
 
 import pipeline.clean
 import pipeline.score
 import pipeline.sprint_schedule
 import pipeline.skills
 from pipeline.etc import daily_piv, time_check, x_Bus_Day_ago, Business_Days
-from pipeline.tables import tables, zipfiles, contact_counts
+from pipeline.tables import tables, compressed_files, contact_counts, extract_file_name
 
-import server.connections, server.insert, server.secret
+import server.connections, server.insert
 import server.queries.MasterSiteId
 import server.queries.reschedule
 import server.queries.optum_assigned
 import server.queries.call_campaign_insert
 from server.insert import clean_for_insert, before_insert, sql_insert
-
 import log.log as log
 
-servername  = server.secret.servername
-database    = server.secret.database
-
-date_format = '%Y-%m-%d'
-BusinessDay = Business_Days
+Bus_day = Business_Days()
 
 def main(test='n', msid='n', sample='n'):
     server_name = 'EUS1PCFSNAPDB01'
@@ -31,18 +25,14 @@ def main(test='n', msid='n', sample='n'):
     dw_engine   = dwworking.create_engine()
 
     ### load & transform
-    if test == 'y':
-        filename = str(f'Call_Campaign_v4_{BusinessDay.yesterday.strftime("%m%d")}*')
-    else:
-        filename = str(f'Call_Campaign_v4_{BusinessDay.today.strftime("%m%d")}*')
-
-    load = zipfiles('pull', 'NA', filename)
+    extract, filename = extract_file_name(test)
+    load = compressed_files(filename, path=extract, sep='|')
     log.df_len('load', load)
 
     ### test last call date = today
-    tested, test = pipeline.clean.check_load(load, BusinessDay.today)
+    tested, test = pipeline.clean.check_load(load, Bus_day.today)
     log.df_len('tested', tested)
-    time_check(BusinessDay.now, f'File Load \t{test}')
+    time_check(Bus_day.now, f'File Load \t{test}')
     
     # ## Reschedules
     # reschedule_sql = server.queries.reschedule.sql()
@@ -61,46 +51,31 @@ def main(test='n', msid='n', sample='n'):
     mastersite = tables('pull',  'na',     'mastersite.csv')
     mapped = pd.merge(tested, mastersite, how='left', on='OutreachID')
     log.df_len('MasterSiteId', mapped)
-    time_check(BusinessDay.now, 'msid map')
+    time_check(Bus_day.now, 'msid map')
 
     ### fix & add columns
-    clean = pipeline.clean.clean(mapped, BusinessDay.tomorrow_str)
+    clean = pipeline.clean.clean(mapped, Bus_day.tomorrow_str)
     log.df_len('clean', clean)
-    time_check(BusinessDay.now, 'clean')
-
-    # optum assigned inventory
-    # optum_assigned_sql = server.queries.optum_assigned.sql()
-    # optum_assigned =  pd.read_sql(optum_assigned_sql, dw_engine)
-    # log.df_len('optum_assigned', optum_assigned)
-    # time_check(BusinessDay.now, 'optum_assigned')
-    # # add filter with above query
-    # org_list = optum_assigned.OutreachID.to_list()
-    # f1 = clean.OutreachID.isin(org_list)
-    # clean['optum_assigned'] = np.where(f1, 1, 0)
-
-    # dft = pd.read_csv('data/table_drop/table.csv')
-    # f1 = clean.OutreachID.isin(dft.OutreachID.to_list())
-    # # f2 = clean.Retrieval_Team  == 'Genpact Offshore'
-    # clean['temp_jen'] = np.where(f1, 1, 0)
+    time_check(Bus_day.now, 'clean')
     
     ### reskill inventory
     skilled = pipeline.skills.complex_skills(clean)
     log.df_len('skilled', skilled)
-    skilled['PhoneNumber'] = skilled['PhoneNumber'].astype(str)
-    time_check(BusinessDay.now, 'skill')
+    time_check(Bus_day.now, 'skill')
 
     ### score inventory per skill
     scored = pipeline.score.split(skilled)
     log.df_len('scored', scored)
-    time_check(BusinessDay.now, 'Split, Score, & Parent/Child Relationship')
+    time_check(Bus_day.now, 'Split, Score, & Parent/Child Relationship')
     
     def Save():
         ### save file
-        zipfiles('push', scored, BusinessDay.tomorrow_str)
+        compressed_files(f"{Bus_day.tomorrow_str}.zip", table=scored)
+        # compressed_files(f"{Bus_day.tomorrow_str}.csv.gz", table=scored)
         ### get column name & types ~ collect unique phone script
         tables('push',  scored.dtypes.reset_index(), 'columns.csv')
         ### reporting
-        time_check(BusinessDay.now, 'Save files')
+        time_check(Bus_day.now, 'Save files')
         ### insert into server ###
         load = clean_for_insert(scored)
         load_date = ''.join(scored.Load_Date.unique())
@@ -109,14 +84,14 @@ def main(test='n', msid='n', sample='n'):
         sql_insert(load, dw_engine, table)
 
         contact_counts(scored)
-        time_check(BusinessDay.now, 'batch_insert')
+        time_check(Bus_day.now, 'batch_insert')
 
     ### create campaign pivot
     daily_piv(scored)
-    time_check(BusinessDay.now, 'Create Pivot Table')
+    time_check(Bus_day.now, 'Create Pivot Table')
 
     if sample == 'y':
-        zipfiles('push', scored, BusinessDay.tomorrow_str)
+        compressed_files(f"{Bus_day.tomorrow_str}.zip", table=scored)
 
     if test == 'Pass': Save() 
 
