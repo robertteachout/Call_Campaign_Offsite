9062828877816c6efc4ad6bcd015a8ac870caeb0
Aaron Roethe <roethe.aaron@outlook.com>
2022-04-12 15:10:50 -0700

    check business lines before custom skill and score

--- src/pipeline/score.py
+++ src/pipeline/score.py
@@ -1,12 +1,9 @@
-import json
-from json.decoder import JSONDecodeError
-
 import numpy as np
 import pandas as pd
 
-from .business_prioirty import ciox_busines_lines
-from .tables import CONFIG_PATH
+from .business_prioirty import ciox_busines_lines, Business_Line
 
+business = ciox_busines_lines() 
 
 def rank(df=pd.DataFrame, new_col=str, groups=list, rank_cols=dict):
     sort_columns = groups + [*rank_cols.keys()]
@@ -18,8 +15,8 @@ def rank(df=pd.DataFrame, new_col=str, groups=list, rank_cols=dict):
     return df
 
 
-def project_rank(df, buz_line, rank, temp):
-    f1 = df.Project_Type.isin(buz_line.projects)
+def project_rank(df, projects, rank, temp):
+    f1 = df.Project_Type.isin(projects)
     df[f"temp_rank{rank}"] = np.where(f1, 1, 0)
     # create dict for scoring
     temp[f"temp_rank{rank}"] = False
@@ -27,19 +24,24 @@ def project_rank(df, buz_line, rank, temp):
 
 
 def stack_inventory(df, grouping):
-    business = ciox_busines_lines()
+    # init temp ranks
     temp = {}
-    for index, buz_line in enumerate(business):
-        # create column on dataframe
-        df, temp = project_rank(df, buz_line, index, temp)
+    # if data available in business_lines.json load into scoring logic
+    if isinstance(business, list):
+    # remove default skills that only need re-scoring
+        re_skill_business = [line for line in business
+                                if line.system != "default"]
+        for index, buz_line in enumerate(re_skill_business):
+            # create column on dataframe
+            df, temp = project_rank(df, buz_line.projects, index, temp)
 
-    # for chartfinder inventory search projects and move to adhoc skills
-    if grouping == "PhoneNumber":
-        for buz_line in business:
-            if buz_line.system == "CC_ChartFinder":
-                f0 = df.Skill == buz_line.system
-                f1 = df.Project_Type.isin(buz_line.projects)
-                df.Skill = np.where(f0 & f1, buz_line.skill, df.Skill)
+        # for chartfinder inventory search projects and move to adhoc skills
+        if grouping == "PhoneNumber":
+            for buz_line in business:
+                if buz_line.system == "CC_ChartFinder":
+                    f0 = df.Skill == buz_line.system
+                    f1 = df.Project_Type.isin(buz_line.projects)
+                    df.Skill = np.where(f0 & f1, buz_line.skill, df.Skill)
 
     rank_cols = {
         "meet_target_sla": True,
@@ -66,23 +68,23 @@ def stack_inventory(df, grouping):
     return full_rank
 
 
-def skill_score(df, skill, skill_rank):
+def skill_score(df, skill, skill_rank:Business_Line):
     # check if skill is active & ranking columns exists
     if df.Skill.isin([skill]).any() and set(skill_rank.keys()).issubset(df.columns):
         skill = df[df.Skill == skill].copy()
         dump = rank(skill, "Score", ["Skill", "parent"], skill_rank)
-        return (
-            pd.concat([dump, df]).drop_duplicates(["OutreachID"]).reset_index(drop=True)
-        )
+        return pd.concat([dump, df])\
+                    .drop_duplicates(["OutreachID"])\
+                    .reset_index(drop=True)
+        
     else:
         print(f"Check Skill: {skill} | validate columns: {set(skill_rank.keys())}")
         raise SystemExit
 
 
-def custom_skills(table, skills):
-    for skill in skills:
-        for skill_name, rank_order in skill.items():
-            table = skill_score(table, skill_name, rank_order)
+def custom_skills(table, business:list[Business_Line]):
+    for line in business:
+        table = skill_score(table, line.skill, line.scoring)
     return table
 
 
@@ -102,21 +104,12 @@ def split(df):
     )
 
     ### skills that need special treatment
-    try:
-        with open(CONFIG_PATH / "custom_skill_rank.json") as json_file:
-            skills = json.load(json_file)
-    except JSONDecodeError:  # includes simplejson.decoder.JSONDecodeError
-        skills = None
-
-    if skills != None:
-        unique = custom_skills(unique, skills)
+    if business != None:
+        score_business = [line for line in business 
+                            if list(line.scoring.keys()) != ["default"]]
+        unique = custom_skills(unique, score_business)
 
     ### Piped ORGs attached to phone numbers
     f0 = unique.Project_Type.isin(["Chart Sync"])  # 'ACA-PhysicianCR'
     unique["Score"] = np.where(f0, 1000000, unique.Score)
     return unique
-
-
-def split_drop_score(df):
-    ### Sort Order and drop Dups
-    return split(df)
