8c7d60342b2c4c24cb6c95bf912d130291431190
Aaron Roethe <roethe.aaron@outlook.com>
2022-04-12 15:18:57 -0700

    msid query, where offsite

--- src/server/queries/MasterSiteId.py
+++ src/server/queries/MasterSiteId.py
@@ -12,22 +12,44 @@ def sql():
     # OR  msi.MSId IS NULL
 
     sql = """
-    SELECT DISTINCT
-         msi.[MSId] MasterSiteId
-        ,msi.outreachid OutreachID
-        ,[SPI]
-        --,[AgentId]
-        ,[Phone] MSI_Phone
-    FROM MasterSiteID.dbo.MSIOutreach MSI
-    LEFT JOIN [MasterSiteID].[dbo].[MSI] ms
-        ON ms.Id = msi.[MSId]
-    LEFT JOIN (
+    WITH offsite AS (
+        SELECT 
+            fc.OutreachId
+        FROM [DW_Operations].[dbo].[FactChart]              fc
+        LEFT JOIN [DW_Operations].[dbo].[DimRetrievalMethod]    rm
+            ON fc.RetrievalMethodId = rm.RetrievalMethodId
+        LEFT JOIN [DW_Operations].[dbo].[DimProjectStatus]      ps
+            ON fc.ProjectStatusId = ps.ProjectStatusId
+        WHERE   rm.RetrievalGroup = 'Offsite'
+        AND     ps.ProjectStatusId IN (1, 3)
+    )
+    , offsite_msid AS (
         SELECT DISTINCT
-        MSId
-        ,Phone
+            msi.[MSId] [MasterSiteId]
+            ,msi.outreachid [OutreachID]
+            ,spi.[SPI]
+        FROM MasterSiteID.dbo.MSIOutreach   msi
+        JOIN offsite                        fc
+            ON msi.outreachid = fc.outreachid
+        LEFT JOIN MasterSiteID.dbo.MSI      spi
+            ON msi.MSId = spi.Id    
+        WHERE msi.MSId <> 1000838 
+    )
+    , msic AS (
+        SELECT DISTINCT
+            MSId
+            ,Phone MSI_Phone
         FROM [MasterSiteID].[dbo].[MSIContact] msic
         WHERE msic.[Primary] = 1
-        ) AS msic
-        ON msic.MSId = msi.[MSId]
+    )
+
+    SELECT DISTINCT
+        MasterSiteId
+        ,OutreachID
+        ,SPI
+        ,MSI_Phone
+    FROM offsite_msid                  msi
+    LEFT JOIN msic
+        ON msi.MasterSiteId = msic.MSId
     """
     return sql
