77d5b2caf980108de6f378849eebbc32820d2714
Aaron Roethe <roethe.aaron@outlook.com>
2022-04-26 23:19:41 -0700

    full rewrite on UI, very dynamic

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
+from sqlalchemy import null, values
 import streamlit as st
 import pandas as pd
-import json
-import log.log as log
-from pipeline.business_prioirty import ciox_busines_lines, Business_Line
+from pipeline.business_prioirty import ciox_busines_lines, Business_Line, write_json
 import pipeline.clean
 import pipeline.score
 import pipeline.skills
@@ -19,53 +18,120 @@ from pipeline.tables import (compressed_files, contact_counts,
 from server.insert import before_insert, clean_for_insert, sql_insert
 
 Bus_day = Business_Days()
-
+# load_file
 extract, filename = extract_file_name('y')
-print(filename)
-load = compressed_files(filename, path=extract, sep="|")
-
+load = compressed_files(filename, path=extract, sep="|").rename(columns=lambda x: x.replace(" ", "_"))
+# saved user input
 inputs = ciox_busines_lines()
-new_skill = Business_Line()
-inputs.append(new_skill)
-
+# get skills & projects apart of the file
 ls_skill = [line.skill for line in inputs]
-projects = load['Project Type'].unique()
-columns  = load.columns
 
+columns_for_input = ["Audit_Type", "Project_Type", "Outreach_Status"]
+unique_items = {item:load[item].unique() for item in columns_for_input}
+columns = list(load.columns)
 
-# with st.form("my_form"):
 add_selectbox = st.sidebar.selectbox(
     "Pick a Custom Skill to custmize",
     ls_skill
 )
+    
+# select skill that need to be changed
+for line in inputs:
+    if line.skill == add_selectbox: select_skill = line
 
-selected_projects = st.multiselect("All Projects", projects)
-selected_columns  = st.multiselect("All columns", columns)
+if st.sidebar.radio(f"Remove {add_selectbox} Skill", ["No","Yes"]) == "Yes":
+    inputs = [line for line in inputs if line.skill != add_selectbox]
+# user input change
+change = st.text_input("Change Skill Name: ", placeholder=select_skill.skill)
 
-# selected_skill = st.selectbox("Custom Skills", ls_skill)
-def df_print(name):
-    for i in inputs:
-        if i.skill == name:
-            st.text(json.dumps(i.as_dict(),indent=4))
-            # st.metric(label="skill", value=i.skill)
-            # st.metric(label="filters", value=str(i.filters))
-            # st.metric(label="new_columns", value=str(i.new_columns))
-            # st.metric(label="scoring", value=str(i.scoring))
+if change:
+    select_skill.skill = change
+
+def make_change(change_type, choice, c):
+    with c:
+        change = st.multiselect(change_type, choice, default=select_skill.filters[change_type])
+        if change:
+            select_skill.filters[change_type] = change
+        return select_skill
+
+# filters
+with st.expander("Choose Filters"):
+    cols = [c for c in st.columns(len(columns_for_input))]
+    for unique, c in zip(unique_items.items(), cols):
+        k, v = unique
+        make_change(k, v, c)
+
+conditions = {
+    "Nan":".isna()",
+    "Not Nan":".notna()",
+
+    "equals": " == ",
+    "not_equals": " != ",
+
+    "contains":".str.contains ",
+    "starts":".str.startswith ",
+    "ends":".str.endswith ",
+
+    "greater": " > ",
+    "greater_than_or_equal": " >= ",
+    "less": " < ",
+    "less_than_or_equal": " <= ",
+}
+
+def conditions_key_wiget(key):
+        
+    col0, col1, col2, col3 = st.columns(4)
+    with col0:
+        if key != 1:
+            if st.button("Remove", key=key): return None
+    with col1:
+        column = st.selectbox("Columns", ["Select"] + columns, key=key, )
+    with col2:
+        cond = st.selectbox("Condition", list(conditions.keys()), key=key)
+    with col3:
+        value = st.text_input("value",key=key)
+
+    if column != "Select":
+        if cond in ("Nan", "Not Nan"):
+            return f"`{column}`{conditions[cond]}"
+        else:
+            return f"`{column}`{conditions[cond]} ({value})"
+
+# If there are multiple conditions, we combine them together, with the
+# given operator in the middle
+OPERATOR_SIGNS = {"Or": " | ", "And": " & "}
 
 
-            # st.dataframe( pd.DataFrame(i.as_dict()).drop('skill',axis=1) )
+def filters(filter, key):
+    if filter != None and key <= 4:
+        key += 1
+        operate = st.selectbox("Operator", OPERATOR_SIGNS.keys() ,key=key)
+        new_filter = conditions_key_wiget(key)
 
-# df_print(add_selectbox)
+        if new_filter != None:
+            combine = f"( {filter} ) {OPERATOR_SIGNS[operate]} ( {new_filter} )"
+            return filters(combine, key)
+        else:
+            return filter
 
-def change_skill_name(inputs, name):
-    for line in inputs:
-        if line.skill == name:
-            new_name = st.text_input("Change Skill Name: ")
-            if new_name != '':
-                line.skill = new_name
-    return inputs
 
-df_print(add_selectbox)
-outputs = change_skill_name(inputs, add_selectbox)
+with st.expander("Sepecial Case"):
+    f1 = conditions_key_wiget(1)
+    filter_stack = filters(f1, 1)
 
+st.write(filter_stack)
+
+
+with st.expander("Choose Scroing, Order Matters!"):
+    columns = columns + list(select_skill.scoring.keys())
+
+    st.multiselect("Scroing", columns, default=list(select_skill.scoring.keys()))
+    st.multiselect("Order: True=ASCD, False=DESC", len(select_skill.scoring.keys())*[True, False], default=list(select_skill.scoring.values()))
+
+output = [line.as_dict() for line in inputs if line.skill != "New_skill"]
+
+st.dataframe(load.query(filter_stack))
+
+if st.sidebar.button(f"Submit Changes"):
+    write_json(output)
 
--- src/pipeline/business_prioirty.py
+++ src/pipeline/business_prioirty.py
@@ -5,14 +5,18 @@ from typing import Dict, List
 from .tables import CONFIG_PATH
 
 name = 'business_lines.json'
+name = 'data.json'
 
 @dataclass
 class Business_Line:
     skill: str              = field(default_factory=lambda: "New_skill")
-    filters: Dict[str,list] = field(default_factory=lambda:{"status":["default"],
-                                                            "equal_1":["default"],
-                                                            "projects":["default"]})
-    new_columns: List       = field(default_factory=lambda: ["default"])
+    filters: Dict[str,list] = field(default_factory=lambda:{
+                                                            "Audit_Type":[],
+                                                            "Project_Type":[],
+                                                            "Outreach_Status":[],
+                                                            "general":[]
+                                                            })
+    new_columns: List       = field(default_factory=lambda: [])
     scoring:Dict[str,bool]  = field(default_factory=lambda:{"meet_target_sla":True, 
                                                             "no_call":False,  
                                                             "age":False})
@@ -24,9 +28,16 @@ def read_json(name):
     with open(CONFIG_PATH / name) as json_file:
             return json.load(json_file)
 
+def write_json(output):
+    with open(CONFIG_PATH / f"test{name}",'w') as file:
+        json.dump(output, file, indent=4)
+
 def ciox_busines_lines() -> list[Business_Line]:
     try:
         data = read_json(name)
-        return [Business_Line(*d.values()) for d in data]
+        new_skill = Business_Line()
+        bus = [Business_Line(*d.values()) for d in data]
+        bus.append(new_skill)
+        return bus
     except:
         return None
