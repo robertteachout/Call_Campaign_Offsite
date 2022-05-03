f9b64b2f724d7f8e2e8e2a20548059b350b9d1cb
Aaron Roethe <roethe.aaron@outlook.com>
2022-05-03 12:12:43 -0700

    score widgets

--- src/app.py
+++ src/app.py
@@ -1,21 +1,13 @@
-from sqlalchemy import null, values
-import streamlit as st
+from typing import final
+
 import pandas as pd
-from pipeline.business_prioirty import ciox_busines_lines, Business_Line, write_json
-import pipeline.clean
-import pipeline.score
-import pipeline.skills
-import pipeline.sprint_schedule
-import server.connections
-import server.insert
-import server.queries.call_campaign_insert
-import server.queries.MasterSiteId
-import server.queries.optum_assigned
-import server.queries.reschedule
-from pipeline.utils import Business_Days, daily_piv, time_check, x_Bus_Day_ago
+import streamlit as st
+
+from pipeline.business_prioirty import (Business_Line, ciox_busines_lines,
+                                        write_json)
 from pipeline.tables import (compressed_files, contact_counts,
                              extract_file_name, tables)
-from server.insert import before_insert, clean_for_insert, sql_insert
+from pipeline.utils import Business_Days, daily_piv, time_check, x_Bus_Day_ago
 
 Bus_day = Business_Days()
 # load_file
@@ -28,7 +20,7 @@ ls_skill = [line.skill for line in inputs]
 
 columns_for_input = ["Audit_Type", "Project_Type", "Outreach_Status"]
 unique_items = {item:load[item].unique() for item in columns_for_input}
-columns = list(load.columns)
+columns = ["Select"] + list(load.columns)
 
 add_selectbox = st.sidebar.selectbox(
     "Pick a Custom Skill to custmize",
@@ -68,34 +60,42 @@ conditions = {
     "equals": " == ",
     "not_equals": " != ",
 
-    "contains":".str.contains ",
-    "starts":".str.startswith ",
-    "ends":".str.endswith ",
-
     "greater": " > ",
     "greater_than_or_equal": " >= ",
     "less": " < ",
     "less_than_or_equal": " <= ",
+
+    "contains":".str.contains ",
+    "starts":".str.startswith ",
+    "ends":".str.endswith ",
 }
 
 def conditions_key_wiget(key):
-        
     col0, col1, col2, col3 = st.columns(4)
+    sign = "nan"
     with col0:
-        if key != 1:
-            if st.button("Remove", key=key): return None
+        if key != 0:
+            operate = st.selectbox("Operator", OPERATOR_SIGNS.keys() ,key=key)
+            sign = OPERATOR_SIGNS[operate]
     with col1:
-        column = st.selectbox("Columns", ["Select"] + columns, key=key, )
+        column = st.selectbox("Columns", columns, key=key, )
     with col2:
         cond = st.selectbox("Condition", list(conditions.keys()), key=key)
     with col3:
         value = st.text_input("value",key=key)
 
+        # if isinstance(value, str): 
+        #     value = f"'{value}'"
+
     if column != "Select":
         if cond in ("Nan", "Not Nan"):
-            return f"`{column}`{conditions[cond]}"
+            filter = f"`{column}`{conditions[cond]}"
+            return sign, filter
         else:
-            return f"`{column}`{conditions[cond]} ({value})"
+            filter =  f"`{column}`{conditions[cond]} ({value})"
+            return sign, filter
+    else:
+        return None, None
 
 # If there are multiple conditions, we combine them together, with the
 # given operator in the middle
@@ -103,34 +103,76 @@ OPERATOR_SIGNS = {"Or": " | ", "And": " & "}
 
 
 def filters(filter, key):
-    if filter != None and key <= 4:
+    if filter and key <= 4:
         key += 1
-        operate = st.selectbox("Operator", OPERATOR_SIGNS.keys() ,key=key)
-        new_filter = conditions_key_wiget(key)
-
-        if new_filter != None:
-            combine = f"( {filter} ) {OPERATOR_SIGNS[operate]} ( {new_filter} )"
+        sign, new_filter = conditions_key_wiget(key)
+        if new_filter:
+            combine = f"( {filter} ) {sign} ( {new_filter} )"
             return filters(combine, key)
         else:
             return filter
 
+def generate_widgets():
+    for i in range(100):
+        yield i
+
+# weg = generate_widgets()
+# def save_temp(a):
+#     with open("file.txt", "w") as f:
+#         f.truncate()
+#         f.write(f"{a}")
+# with open("file.txt", "r") as f:
+#     a = f.readline()  # starts as a string
+#     a = 0 if a == "" else int(a)  # check if its an empty string, otherwise should be able to cast using int()
+
+# if st.button("add filter"):
+#     st.write(a)
+#     a += 1
+# if st.button("remove filter"):
+#     st.write(a)
+#     a -= 1
+
 
 with st.expander("Sepecial Case"):
-    f1 = conditions_key_wiget(1)
+    _, f1 = conditions_key_wiget(0)
     filter_stack = filters(f1, 1)
+    # if filter_stack:
+    #     # st.write(filter_stack)
+    #     # st.dataframe(load.query(filter_stack))
+    # else:
+    #     st.dataframe(load)
 
-st.write(filter_stack)
-
+orders = {True:"Ascending", False:"Descending"}
 
 with st.expander("Choose Scroing, Order Matters!"):
     columns = columns + list(select_skill.scoring.keys())
+    scoring = select_skill.scoring 
+    # scoring["Select"] = True
 
-    st.multiselect("Scroing", columns, default=list(select_skill.scoring.keys()))
-    st.multiselect("Order: True=ASCD, False=DESC", len(select_skill.scoring.keys())*[True, False], default=list(select_skill.scoring.values()))
+    def score(key, column, order):
+        col1, col2 = st.columns(2)
+        res = 0
+        with col1:
+            for x, z in enumerate(columns):
+                if z == column:
+                    res = x
+            select_col = st.selectbox("Scroing", columns, key=key, index=res)
+        with col2:
+            for x, z in enumerate(orders):
+                if z == order:
+                    res = x
+            select_order = st.selectbox("Order", ["Ascending", "Descending"], index=res, key=key)
+        return select_col, select_order
 
+    new_score = {}
+    for idx, s in enumerate(scoring.items()):
+        k , v = score(idx, *s)
+        new_score[k] = v
+
+    st.write(new_score)
+    
 output = [line.as_dict() for line in inputs if line.skill != "New_skill"]
 
-st.dataframe(load.query(filter_stack))
 
 if st.sidebar.button(f"Submit Changes"):
     write_json(output)
