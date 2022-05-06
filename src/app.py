6524b77151a3d754633555bfa247550d94a776e4
Aaron Roethe <roethe.aaron@outlook.com>
2022-05-06 09:08:56 -0700

    list_to_string parser

--- src/app.py
+++ src/app.py
@@ -1,13 +1,10 @@
-from typing import final
-
 import pandas as pd
 import streamlit as st
 
-from pipeline.business_prioirty import (Business_Line, ciox_busines_lines,
-                                        write_json)
-from pipeline.tables import (compressed_files, contact_counts,
-                             extract_file_name, tables)
-from pipeline.utils import Business_Days, daily_piv, time_check, x_Bus_Day_ago
+from front_end import sidebar
+from pipeline.business_prioirty import ciox_busines_lines, write_json
+from pipeline.tables import compressed_files, extract_file_name
+from pipeline.utils import Business_Days
 
 Bus_day = Business_Days()
 # load_file
@@ -22,18 +19,8 @@ columns_for_input = ["Audit_Type", "Project_Type", "Outreach_Status"]
 unique_items = {item:load[item].unique() for item in columns_for_input}
 columns = ["Select"] + list(load.columns)
 
-add_selectbox = st.sidebar.selectbox(
-    "Pick a Custom Skill to custmize",
-    ls_skill
-)
-    
-# select skill that need to be changed
-for line in inputs:
-    if line.skill == add_selectbox: select_skill = line
+select_skill, inputs = sidebar.add_wegits(ls_skill, inputs)
 
-if st.sidebar.radio(f"Remove {add_selectbox} Skill", ["No","Yes"]) == "Yes":
-    inputs = [line for line in inputs if line.skill != add_selectbox]
-# user input change
 change = st.text_input("Change Skill Name: ", placeholder=select_skill.skill)
 
 if change:
@@ -54,11 +41,11 @@ with st.expander("Choose Filters"):
         make_change(k, v, c)
 
 conditions = {
-    "Nan":".isna()",
-    "Not Nan":".notna()",
-
     "equals": " == ",
     "not_equals": " != ",
+
+    "Nan":".isna()",
+    "Not Nan":".notna()",
 
     "greater": " > ",
     "greater_than_or_equal": " >= ",
@@ -70,30 +57,36 @@ conditions = {
     "ends":".str.endswith ",
 }
 
+def list_to_string(column, condition, value):
+    if condition in ("Nan", "Not Nan"):
+        filter = f"`{column}`{conditions[condition]}"
+        return filter
+    else:
+        filter =  f"`{column}`{conditions[condition]} ({value})"
+        return filter
+
+def sort_selectbox(column, columns):
+    for idx, item in enumerate(columns):
+        if item == column:
+            target = idx
+    return target
+
 def conditions_key_wiget(key):
     col0, col1, col2, col3 = st.columns(4)
-    sign = "nan"
+    operator = None
     with col0:
         if key != 0:
-            operate = st.selectbox("Operator", OPERATOR_SIGNS.keys() ,key=key)
-            sign = OPERATOR_SIGNS[operate]
+            operator_wiget = st.selectbox("Operator", OPERATOR_SIGNS.keys(), key=key)
+            operator = OPERATOR_SIGNS[operator_wiget]
     with col1:
-        column = st.selectbox("Columns", columns, key=key, )
+        column = st.selectbox("Columns", columns, key=key )
     with col2:
-        cond = st.selectbox("Condition", list(conditions.keys()), key=key)
+        condition = st.selectbox("Condition", list(conditions.keys()), key=key)
     with col3:
         value = st.text_input("value",key=key)
 
-        # if isinstance(value, str): 
-        #     value = f"'{value}'"
-
     if column != "Select":
-        if cond in ("Nan", "Not Nan"):
-            filter = f"`{column}`{conditions[cond]}"
-            return sign, filter
-        else:
-            filter =  f"`{column}`{conditions[cond]} ({value})"
-            return sign, filter
+        return operator, list_to_string(column, condition, value)
     else:
         return None, None
 
@@ -105,42 +98,18 @@ OPERATOR_SIGNS = {"Or": " | ", "And": " & "}
 def filters(filter, key):
     if filter and key <= 4:
         key += 1
-        sign, new_filter = conditions_key_wiget(key)
+        operator, new_filter = conditions_key_wiget(key)
         if new_filter:
-            combine = f"( {filter} ) {sign} ( {new_filter} )"
+            combine = f"( {filter} ) {operator} ( {new_filter} )"
             return filters(combine, key)
         else:
             return filter
 
-def generate_widgets():
-    for i in range(100):
-        yield i
-
-# weg = generate_widgets()
-# def save_temp(a):
-#     with open("file.txt", "w") as f:
-#         f.truncate()
-#         f.write(f"{a}")
-# with open("file.txt", "r") as f:
-#     a = f.readline()  # starts as a string
-#     a = 0 if a == "" else int(a)  # check if its an empty string, otherwise should be able to cast using int()
-
-# if st.button("add filter"):
-#     st.write(a)
-#     a += 1
-# if st.button("remove filter"):
-#     st.write(a)
-#     a -= 1
-
-
 with st.expander("Sepecial Case"):
     _, f1 = conditions_key_wiget(0)
     filter_stack = filters(f1, 1)
-    # if filter_stack:
-    #     # st.write(filter_stack)
-    #     # st.dataframe(load.query(filter_stack))
-    # else:
-    #     st.dataframe(load)
+
+    st.write(filter_stack)
 
 orders = {True:"Ascending", False:"Descending"}
 
@@ -151,17 +120,13 @@ with st.expander("Choose Scroing, Order Matters!"):
 
     def score(key, column, order):
         col1, col2 = st.columns(2)
-        res = 0
+        idx = 0
         with col1:
-            for x, z in enumerate(columns):
-                if z == column:
-                    res = x
-            select_col = st.selectbox("Scroing", columns, key=key, index=res)
+            idx = sort_selectbox(column, columns)
+            select_col = st.selectbox("Scroing", columns, key=key, index= idx)
         with col2:
-            for x, z in enumerate(orders):
-                if z == order:
-                    res = x
-            select_order = st.selectbox("Order", ["Ascending", "Descending"], index=res, key=key)
+            idx = sort_selectbox(orders, order)
+            select_order = st.selectbox("Order", ["Ascending", "Descending"], index=idx, key=key)
         return select_col, select_order
 
     new_score = {}
